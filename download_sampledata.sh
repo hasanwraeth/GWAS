46bce3e1bc50dbfb91a10e0e2a7925a7ee827fdb
Hasan Al Reza, PhD <72579193+hasanwraeth@users.noreply.github.com>
2024-04-16 14:46:42 -0400

    Add files via upload

---
+++ download_sampledata.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O 1KG.EAS.auto.snp.norm.nodup.split.rare002.common015.missing.zip https://www.dropbox.com/scl/fi/41ep8xbdccp9xw5epim19/1KG.EAS.auto.snp.norm.nodup.split.rare002.common015.missing.zip?rlkey=tklapxwypeg79b1sx03o6ycs7&dl=1
+unzip -j 1KG.EAS.auto.snp.norm.nodup.split.rare002.common015.missing.zip
